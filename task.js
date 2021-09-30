f742e08147c8ee00ccaf54bbab8d3eb82e368d71
AliZad64 <78386181+AliZad64@users.noreply.github.com>
2021-09-30 19:27:44 +0300

    adjusting comments

--- task.js
+++ task.js
@@ -13,7 +13,7 @@ let numbers = [
 // 3. we clone our array to another value so the numbers array stay the same
 change_numbers = [...numbers];
 
-//creating function for ascending sort
+//2. using functions is better so we don't need to repeat the code
 function Bubble_Sort_Asc(numbers)
 {
 change_numbers = numbers
@@ -21,7 +21,7 @@ for(let i = 0 ; i < numbers.length ; i++)
 {
     for(let j = 0 ; j < numbers.length - 1 ; j++)
     {   /* 
-            2. the fix in code was in if condition of both sort methods because "i" won't move
+            1. the fix in code was in if condition of both sort methods because "i" won't move
             until j finishes its loop which is against the bubble sort algorithm rules
             */
         if(numbers[j] > numbers[j+1])
@@ -41,7 +41,7 @@ for(let i = 0 ; i < numbers.length ; i++)
 {
     for(let j = 0 ; j < numbers.length - 1 ; j++)
     {   /* 
-            2. the fix in code was in if condition of both sort methods because "i" won't move
+            1. the fix in code was in if condition of both sort methods because "i" won't move
             until "j" finishes its loop which is against the bubble sort algorithm rules
             */
         if(numbers[j] < numbers[j+1])
@@ -61,6 +61,7 @@ console.log("Numbers list After Desc sorting: ", Bubble_Sort_Desc(change_numbers
 
 
 /*
+4.
 1- read the data.tx file and split into array
 2- do ascending bubble sort to the array
 3- create and write the file to output.txt
